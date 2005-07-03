5d1124c47b1a57ebf40b7c669118fc69cd87307b
florian <florian@fbb464cd-0adc-0310-b34b-a1d531edae02>
2005-07-03 01:46:41 +0000

    Java 1.5 hat wohl Probleme mit Umlauten, auch in Kommentaren, deshalb: ü -> ue

--- tools/mksl3/mksl3.java
+++ tools/mksl3/mksl3.java
@@ -105,7 +105,7 @@ public class mksl3 {
 			validate=true;
 		}
 		
-		// Überpruefe Versionsnummer
+		// Ueberpruefe Versionsnummer
 		if (version==null || version=="") {
 			SimpleDateFormat SLDateFormat = new SimpleDateFormat("yyyyMMdd");
 			Date date = new Date();
